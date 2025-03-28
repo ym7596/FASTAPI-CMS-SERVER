d6f993643a0bc606f77a3335d42ec1e337d3ec08
FatRaccoon <ym7596@naver.com>
2025-03-28 21:44:34 +0900

    feat: ConfigDict 수정

--- app/schema/category.py
+++ app/schema/category.py
@@ -5,54 +5,50 @@ from pydantic import BaseModel, ConfigDict
 
 
 class SubCategoryCreate(BaseModel):
-    model_config = ConfigDict(from_attributes=True)
+    model_config = ConfigDict(
+        from_attributes=True,
+        json_schema_extra= {
+            "example": {
+                "subcategory_name": "수도",
+                "category_id": 1
+                }
+            }
+        )
     subcategory_name: str
     category_id: int
 
-    model_config = {
-        "json_schema_extra": {
-            "example": {
-                "subcategory_name": "subcategory_name",
-                "category_id": 1
-            }
-        }
-    }
 
 class SubCategoryResponse(BaseModel):
-    model_config = ConfigDict(from_attributes=True)
+    model_config = ConfigDict(
+    from_attributes=True,
+    json_schema_extra= {
+        "example": {
+            "subcategory_id": 1,
+            "subcategory_name": "수도",
+            "category_id": 1
+            }
+        }
+    )
     subcategory_id: int
     subcategory_name: str
 
-    model_config = {
-        "json_schema_extra": {
-            "example": {
-                "subcategory_id": 1,
-                "subcategory_name": "수도",
-                "category_id": 1
-            }
-        }
-    }
 
 class CategoryCreate(BaseModel):
-    model_config = ConfigDict(from_attributes=True)
-    name: str
-
-    model_config = {
-        "json_schema_extra": {
+    model_config = ConfigDict(
+        from_attributes=True,
+        json_schema_extra= {
             "example": {
                 "name": "주방"
+                }
             }
-        }
-    }
+        )
+    name: str
+
 
 class CategoryResponse(BaseModel):
-    model_config = ConfigDict(from_attributes=True)
-    category_id: int
-    category_name: str  
-    subcategories: list[SubCategoryResponse]
-
-    model_config = {
-        "json_schema_extra": {
+    model_config = ConfigDict(
+        from_attributes=True,
+        json_schema_extra= {
             "example": {
                 "category_id": 1,
                 "category_name": "주방",
@@ -65,4 +61,7 @@ class CategoryResponse(BaseModel):
                 ]
             }
         }
-    }
+        )
+    category_id: int
+    category_name: str  
+    subcategories: list[SubCategoryResponse]
